6cd4d642752bb52883a65852d6cbcdac0797490d
Yogesh Patel <yogeshpatel5117@gmail.com>
2025-08-28 02:39:25 +0530

    changes in the select statemtns file

--- selectStatements.sql
+++ selectStatements.sql
@@ -23,3 +23,11 @@ WHERE CITY REGEXP "^[AEIOU]";
 SELECT * FROM CITY WHERE 
 COUNTRYCODE = "USA" AND 
 POPULATION > 100000 ;
+
+
+-- Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+
+select name from city
+where countrycode = "USA" 
+and 
+population > 120000;
